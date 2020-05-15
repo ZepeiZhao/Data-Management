e869649fe31aa5c08d908f315ebeaed90e262047
ZepeiZhao <55176816+ZepeiZhao@users.noreply.github.com>
2020-05-14 19:30:49 -0700

    Delete q2_d.sql

--- q2_d.sql
+++
@@ -1,6 +0,0 @@
-SELECT Name 
-FROM country c LEFT OUTER JOIN countrylanguage cl ON c.Code = cl.CountryCode
-WHERE IsOfficial = 'F' 
-GROUP BY CountryCode 
-HAVING COUNT(Language)>=10 
-ORDER BY COUNT(Language) DESC;
