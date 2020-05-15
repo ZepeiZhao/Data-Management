5b18b96c116a7f1bac596f993ccd316468f815c1
ZepeiZhao <55176816+ZepeiZhao@users.noreply.github.com>
2020-05-14 19:31:02 -0700

    Delete q2_f.sql

--- q2_f.sql
+++
@@ -1,6 +0,0 @@
-SELECT Language
-FROM country c,countrylanguage cl
-WHERE c.Code = cl.CountryCode AND IsOfficial = 'T' AND Population > 1000000
-GROUP BY Language
-ORDER BY SUM(Population) DESC
-LIMIT 10;
